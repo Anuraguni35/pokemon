67a741a5c9af379ee04dfd0847405120f58c69ec
Anurag Uniyal <105287057+Anuraguni35@users.noreply.github.com>
2026-03-02 08:07:07 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,42 @@
 "use client";
-import Navbar from "@/components/ui/Navbar";
-import axios from "axios";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React from "react";
 
-
-type Pokemon = {
-  name: string;
- 
-};
 function Home() {
+  const json = {
+    event: {
+      name: "Holi Madness 🎨🔥",
+      type: "Daruuu... Attack Party 💥",
+      festival: "Holi 🌈",
+      description:
+        "Warning ⚠️: You WILL get colored! Join us for madness, music, Daruuuu.... and zero regrets 😎💃🕺",
+    },
+    host: {
+      name: "Sahil (Chief Color Officer) 😜",
+      location: "Sahil ki haveli",
+    },
+    date_time: {
+      date: "2026-03-14 📅",
+      start_time: "10:00 AM ⏰",
+      end_time: "Till We Drop 💃🕺",
+    },
+    rsvp: {
+      required: true,
+      deadline: "2026-02-10 ⏳",
+      contact_method: "Call, Text, DM, or Send a Pigeon 🐦",
+    },
+    additional_notes: [
+      "Daru daba kr (No cheating with Nashe 😜).",
+      "Parking available 🚗",
+      "Bring energy, leave ego at home 😌",
+      "Friends allowed. Enemies… optional 😏",
+    ],
+  };
 
-  const json= {
-    "name": "Holi Madness 🎨🔥",
-    "type": "Daruuu... Attack Party 💥",
-    "festival": "Holi 🌈",
-    "description": "Warning ⚠️: You WILL get colored! Join us for madness, music, Daruuuu.... and zero regrets 😎💃🕺"
-  },
-  "host": {
-    "name": "Sahil (Chief Color Officer) 😜",
-    "location": "Sahil ki haveli",
-  },
-  "date_time": {
-    "date": "2026-03-14 📅",
-    "start_time": "10:00 AM ⏰",
-    "end_time": "Till We Drop 💃🕺",
-  },
- "rsvp": {
-    "required": true,
-    "deadline": "2026-02-10 ⏳",
-    "contact_method": "Call, Text, DM, or Send a Pigeon 🐦"
-  },
-  "additional_notes": [
-    "Daru daba kr (No cheating with Nashe 😜).",
-    "Parking available 🚗",
-    "Bring energy, leave ego at home 😌",
-    "Friends allowed. Enemies… optional 😏"
-  ]
-});
   return (
     <div>
-      "event":{json}
-    </div>)
+      <pre>{JSON.stringify(json, null, 2)}</pre>
+    </div>
+  );
 }
-
 
 export default Home;
